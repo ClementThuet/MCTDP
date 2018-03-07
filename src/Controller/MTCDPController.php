a16e02c4740a4dcc09f04797944c44a1884f9eff
Clement <clementthuet7@gmail.com>
2018-03-07 12:46:56 +0100

    Mise en place de la structure, 1 route, 1 controller index, 1 index.html.twig

---
+++ src/Controller/MTCDPController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Description of MTCDPController
+ *
+ * @author MrClément
+ */
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MTCDPController extends Controller{
+    
+    public function index()
+    {
+       $number = mt_rand(0, 100);
+
+        return $this->render('base.html.twig', array(
+            'number' => $number,
+        ));
+    }
+}
